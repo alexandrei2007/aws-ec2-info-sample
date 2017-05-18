ff38f87dd1b1254b7742e716ec636b8098f98a27
Alexandre Ito <alexandreh.ito@gmail.com>
2017-05-17 21:03:33 -0300

    Initial commit

---
+++ Sample/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.Util;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var properties = new Dictionary<string, object>();
+            properties.Add("MachineName", System.Environment.MachineName);
+            properties.Add("InstanceId", EC2InstanceMetadata.InstanceId);
+            properties.Add("InstanceType", EC2InstanceMetadata.InstanceType);
+            properties.Add("AmiId", EC2InstanceMetadata.AmiId);
+
+            return View(properties);
+        }
+    }
+}
